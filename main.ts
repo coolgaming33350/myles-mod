b76a05bbcfc186da85e2e2d3c058a2ef102361d1
korvwr <130167361+korvwr@users.noreply.github.com>
2023-04-11 13:46:17 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,24 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`Tank Bullet`, mySprite, 50, 0)
 })
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    mySprite.setPosition(0, 0)
+    statusbar.value = 100
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-	
+    statusbar.value += -2.5
 })
 info.onLifeZero(function () {
     sprites.destroy(mySprite, effects.fire, 5000)
     mySprite = sprites.create(assets.image`Tank`, SpriteKind.Player)
 })
 let projectile: Sprite = null
+let statusbar: StatusBarSprite = null
 let mySprite: Sprite = null
+let tilemap2 = 0
 mySprite = sprites.create(assets.image`Tank`, SpriteKind.Player)
 tiles.setCurrentTilemap(tilemap`level1`)
-info.setLife(3)
-let tilemap2 = 0
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
+statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar.attachToSprite(mySprite)
