0d0fe63df53bf96187e867aae697ebce59ddfa20
Mogamer2012 <130108717+Mogamer2012@users.noreply.github.com>
2023-04-12 13:17:23 -0600

    Cool plane Update Coming Soon Boats

--- main.ts
+++ main.ts
@@ -5,15 +5,24 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Vehicle, function (sprite, other
     game.showLongText("PLANE INCOMING", DialogLayout.Bottom)
     sprites.destroy(mySprite)
     mySprite = sprites.create(assets.image`Bomber plane`, SpriteKind.Player)
-    controller.moveSprite(mySprite)
+    controller.moveSprite(mySprite, 200, 200)
     scene.cameraFollowSprite(mySprite)
+    statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+    statusbar.attachToSprite(mySprite)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`Tank Bullet`, mySprite, 50, 0)
 })
 statusbars.onZero(StatusBarKind.Health, function (status) {
+    sprites.destroy(mySprite)
+    mySprite = sprites.create(assets.image`Tank`, SpriteKind.Player)
     mySprite.setPosition(5, 3)
+    pause(5000)
+    statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+    statusbar.attachToSprite(mySprite)
     statusbar.value = 100
+    controller.moveSprite(mySprite)
+    scene.cameraFollowSprite(mySprite)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     statusbar.value += -2.5
@@ -22,16 +31,26 @@ info.onLifeZero(function () {
     sprites.destroy(mySprite, effects.fire, 5000)
     mySprite = sprites.create(assets.image`Tank`, SpriteKind.Player)
 })
+mp.onControllerEvent(ControllerEvent.Connected, function (player2) {
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Tank`, SpriteKind.Player))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Tank`, SpriteKind.Player))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`Tank`, SpriteKind.Player))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`Tank`, SpriteKind.Player))
+    mp.setPlayerIndicatorsVisible(true)
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four))
+})
 let projectile: Sprite = null
 let statusbar: StatusBarSprite = null
 let mySprite: Sprite = null
 let tilemap2 = 0
-mySprite = sprites.create(assets.image`Tank`, SpriteKind.Player)
 tiles.setCurrentTilemap(tilemap`level1`)
-controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 statusbar = statusbars.create(20, 4, StatusBarKind.Health)
 statusbar.attachToSprite(mySprite)
+controller.moveSprite(mySprite)
 mySprite.setPosition(0, 0)
 let Plane = sprites.create(img`
     6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
